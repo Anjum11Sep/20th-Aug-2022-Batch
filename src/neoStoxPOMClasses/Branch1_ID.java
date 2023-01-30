99f741ed2d56fdcebda96ea8fedc5d49a7ddddaf
Anjum <shaikhanjum2222@gmail.com>
2023-01-30 21:57:53 +0530

    Branch ID added

---
+++ src/neoStoxPOMClasses/Branch1_ID.java
@@ -0,0 +1,33 @@
+package neoStoxPOMClasses;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Branch1_ID 
+{
+	 @FindBy(xpath = "(//input[@type='number'])[1]") private WebElement MobileNumField;
+		
+		@FindBy(id = "lnk_signup1") private WebElement SignInButton;
+		
+		//2.
+		
+		public Branch1_ID(WebDriver driver)
+		{
+			PageFactory.initElements(driver, this);
+		}
+		
+		//3.
+		
+		public void EnterMobileNumber()
+		{
+			MobileNumField.sendKeys("7264862687");
+		}
+		
+		public void ClickOnSignInButton()
+		{
+			SignInButton.click();
+		}
+
+}
