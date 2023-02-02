abe7d44863b8a88e431cf00c5145b72fe4f81576
Anjum <shaikhanjum2222@gmail.com>
2023-02-02 07:11:45 +0530

    added branch2class

---
+++ src/neoStoxPOMClasses/Branch2Class.java
@@ -0,0 +1,15 @@
+package neoStoxPOMClasses;
+
+public class Branch2Class {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		
+		System.out.println("addition is"+(a+b));
+
+	}
+
+}
